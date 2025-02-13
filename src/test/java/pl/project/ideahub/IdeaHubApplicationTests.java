f93f4298e6e0b59a79243910e032b8af7e786b53
ajackowska <jackowska.agnieszka@o2.pl>
2025-02-13 03:28:50 +0100

    Initial commit

---
+++ src/test/java/pl/project/ideahub/IdeaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.ideahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
