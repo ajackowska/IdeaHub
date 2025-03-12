797c4a3e2fd5d204bda8365ca1784cc6be39706e
ajackowska <jackowska.agnieszka@o2.pl>
2025-03-13 00:11:07 +0100

    Added configuration

--- src/main/java/pl/project/ideahub/category/domain/model/Category.java
+++ src/main/java/pl/project/ideahub/category/domain/model/Category.java
@@ -1,9 +1,16 @@
 package pl.project.ideahub.category.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "categories")
 public class Category {
 
+    @Id
     private UUID id;
 
     private String name;
